1d80f56395cb60d838360c72df14d969f4dd0c79
Wiem Trabelsi <wiem2007@gmail.com>
2021-04-29 23:58:39 +0200

    morpion

--- lib/bird_case.rb
+++
@@ -1,9 +0,0 @@
-class BoardCase
-    #TO DO : la classe a 2 attr_accessor, sa valeur en string (X, O, ou vide), ainsi que son identifiant de case
-  
-    
-    def initialize
-      #TO DO : doit régler sa valeur, ainsi que son numéro de case
-    end
-    
-  end
